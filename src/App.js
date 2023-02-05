938546b7099a960cb0cc62b3b9468075d592ad4d
PrachiiGuptaa <119650267+PrachiiGuptaa@users.noreply.github.com>
2023-02-05 21:17:35 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Calc from './Calc/Calc'
+
+
+function App() {
+  return (<>
+    <div className="App">
+
+    <h1> CALCULATOR APP</h1>
+           <Calc/>
+
+    </div>
+    </>
+  );
+}
+
+export default App;
